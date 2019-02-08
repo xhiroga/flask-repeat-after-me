ea2be3be37b4301d5ad572ee6c72febbc5423e1e
Hiroaki Ogasawara <hiroga1030@gmail.com>
2019-02-08 17:51:50 +0900

    fix to show url

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello1():
     print(request.headers)
-    print(request.full_path)
-    return request.full_path + "<br>" + "<br>".join([": ".join(header) for header in request.headers.to_wsgi_list()])
+    print(request.url)
+    return request.url + "<p>" + "<br>".join([": ".join(header) for header in request.headers.to_wsgi_list()])
 
 @app.route("/<any>")
 def any(any):
     print(request.headers)
-    print(request.full_path)
-    return request.full_path + "<br>" + "<br>".join([": ".join(header) for header in request.headers.to_wsgi_list()])
+    print(request.url)
+    return request.url + "<p>" + "<br>".join([": ".join(header) for header in request.headers.to_wsgi_list()])
 
 if __name__ == "__main__":
     print("⚡️⚡️⚡️ Flask launch! ⚡️⚡️⚡️")
