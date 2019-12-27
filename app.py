7678064755bfc2176c7f4a2e963b1a35607e430b
Hiroaki Ogasawara <hiroga1030@gmail.com>
2019-12-28 08:30:12 +0900

    lint and show more info

--- app.py
+++ app.py
@@ -1,18 +1,42 @@
 from flask import Flask, request
+
 app = Flask(__name__)
 
+
 @app.route("/", methods=['GET', 'POST'])
-def hello():
+def route():
     return make_response(request)
+
 
 @app.route("/<any>", methods=['GET', 'POST'])
 def any(any):
     return make_response(request)
 
+
 def make_response(req):
-    res = req.url + "\n\n" + "\n".join([": ".join(header) for header in req.headers.to_wsgi_list()]) + "\n\n" + req.data.decode(encoding='utf-8')
+    headers = '\n'.join([": ".join(header) for header in req.headers.to_wsgi_list()])
+    body = req.data.decode(encoding='utf-8')
+    res = f"""# URL
+{req.url}
+
+# Headers
+{headers}
+
+# Body
+{body}
+
+# Referrer
+{req.referrer}
+
+# Remote Host
+{req.remote_addr}
+
+# UserAgent
+{req.user_agent}
+"""
     print(res)
     return res.replace("\n\n", "<p>").replace("\n", "<br>")
+
 
 if __name__ == "__main__":
     print("⚡️⚡️⚡️ Flask launch! ⚡️⚡️⚡️")
