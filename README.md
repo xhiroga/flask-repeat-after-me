0010b5809ef0b2cb810b4c8faf22647502e931cf
xhiroga <13391129+xhiroga@users.noreply.github.com>
2022-01-20 10:58:32 +0900

    feat: with poetry

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ https://flask-repeat-after-me.herokuapp.com/
 
 ## Docker Hub
 https://hub.docker.com/repository/docker/hiroga/flask-repeat-after-me
+
+## Debug
+
+```bash
+poetry shell
+gunicorn app:app --log-file=-
+```
--- app.py
+++ app.py
@@ -4,18 +4,20 @@ from flask import Flask, request
 from werkzeug.routing import Rule
 
 app = Flask(__name__)
-app.url_map.add(Rule('/', defaults={'any': ''},endpoint='index'))
-app.url_map.add(Rule('/<path:any>', endpoint='any'))
+app.url_map.add(Rule("/", defaults={"any": ""}, endpoint="index"))
+app.url_map.add(Rule("/<path:any>", endpoint="any"))
 
-@app.endpoint('index')
-@app.endpoint('any')
+
+@app.endpoint("index")
+@app.endpoint("any")
 def route(any):
     return make_response(request)
 
 
-def make_response(req):
-    headers = '\n'.join([": ".join(header) for header in req.headers.to_wsgi_list()])
-    body = req.data.decode(encoding='utf-8')
+def make_response(req: request):
+    headers = "\n".join([": ".join(header) for header in req.headers.to_wsgi_list()])
+    body = req.data.decode(encoding="utf-8")
+    form = req.form
     res = f"""# URL
 {req.url}
 
@@ -40,4 +42,4 @@ def make_response(req):
 
 if __name__ == "__main__":
     print("⚡️⚡️⚡️ Flask launch! ⚡️⚡️⚡️")
-    app.run(host='0.0.0.0', port=os.environ.get("PORT", 5000), debug=True)
+    app.run(host="0.0.0.0", port=os.environ.get("PORT", 5000), debug=True)
